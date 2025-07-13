facf96cdb7deab1cf8232777ccbadb69e1df49a2
CJ <christianjayduque@gmail.com>
2025-07-13 18:22:17 -0500

    Fixed password hash issue

--- todo/models.py
+++ todo/models.py
@@ -8,7 +8,7 @@ class User(UserMixin, db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
     # Relationship to tasks
